1b3b982c5eb4dd2825ba8c15764cf8bb719f3043
Alton Schultheis <alton.schultheis.external@gmail.com>
2023-04-14 15:22:22 -0400

    adding some dummy lines in placeholder functions

--- patternSpreadsheetMaker.py
+++ patternSpreadsheetMaker.py
@@ -7,15 +7,16 @@ from uniqueReadableData import makeReadableString, makeRandomInt, makeRandomReal
 
 # Parameters
 #where to find pattern(s) JSON
-#pattern_loc = r"C:\Users\aschultheis3\Box\General\_Repos\rapid-modeling-tools\ingrid\src\model_processing\patterns"
 #pattern_loc = r"C:\Users\aschultheis3\Box\General\_Repos\LIPFork-rapid-modeling-tools\ingrid\src\model_processing\patterns"
-pattern_loc = r"C:\Users\aschultheis3\Box\General\MSFTB\Pattern Development\New Patterns"
+#pattern_loc = r"C:\Users\aschultheis3\Box\General\MSFTB\Pattern Development\New Patterns"
+pattern_loc = r"C:\Users\aschultheis3\Box\General\RMT Testing\Pattern Tests\TEMP patterns to read in"
+
 
 #where to put the output files (set as "" to use pattern_loc)
 #output_loc = r"C:\Users\aschultheis3\Box\General\RMT Testing\Pattern Tests"
-output_loc = r"C:\Users\aschultheis3\Box\General\RMT Testing\Pattern Tests\New Pattern Test Spreadsheets"
+output_loc = r"C:\Users\aschultheis3\Box\General\RMT Testing\Pattern Tests\mostly working test set A"
 
-rows = 1 #number of rows to randomly populate for pattern(s)
+rows = 10 #number of rows to randomly populate for pattern(s)
 
 # Defs 
 
@@ -61,11 +62,15 @@ def populateOtherData():
 
 		# use uniqueReadableData.makeRandomInt()
 
+		a = 1
+
 	elif dataType == 'Real':
 		# TODO: random Real in specified precision
 		# excel might to weird things to this, tbd
 
 		# use uniqueReadableData.makeRandomReal()
+
+		a = 1
 
 	elif dataType == 'Complex':
 		# TODO (eventually): random Complex Number
@@ -73,11 +78,15 @@ def populateOtherData():
 
 		# use uniqueReadableData.makeRandomReal() to populate each part of the complex number
 
+		a = 1
+
 	elif dataType == 'Boolean':
 		# TODO (eventually): random Boolean
 		# these will be for however MagicDraw/Cameo expects booleans to be populated, need to figure that out
 
 		# just do this one here since it will likely just be choosing from two strings
+
+		a = 1
 	#
 	#
 
